eb892f685fd676313f19763bcce2c86389cbbec5
RahulPawar1212 <rahul.pawar@nepa.com>
2023-09-14 13:19:46 +0530

    Add better example to add nodes in xml

--- src/components/updateInstructions.ts
+++ src/components/updateInstructions.ts
@@ -7,19 +7,34 @@ export function updateInstructions(data:string){
 
     const parser = new DOMParser();
     const xmlDoc = parser.parseFromString(data, "text/xml");
+    var i = 0;
+
+    for (var Q of _data) {
+      const newQEle = xmlDoc.createElement("NewQuestion"); // Create new node 'NewQuestion'  
+      xmlDoc.getElementsByTagName("persons")[0].appendChild(newQEle) // Add new node to root node 'Persons'
+
+      const newQidEle = xmlDoc.createElement("Qid"); // Create new node 'Qid'
+      xmlDoc.getElementsByTagName("NewQuestion")[i].appendChild(newQidEle) // Add data to new node 
+      
+      xmlDoc.getElementsByTagName("NewQuestion")[i].getElementsByTagName("Qid")[0].textContent = Q.id
+
+      console.log(i);
+      console.log(Q.id)
+      i++
+    }
   
     // Your XML update instructions go here
     // For example, updating the text of an element:
-    const firstElement = xmlDoc.querySelector("person");
-    if (firstElement) {
-      firstElement.textContent = _data[0].answersRowOptions[0].toString() ;
-    }
+    // const firstElement = xmlDoc.querySelector("person");
+    // if (firstElement) {
+    //   firstElement.textContent = _data[0].answersRowOptions[0].toString() ;
+    // }
   
-    xmlDoc.getElementsByTagName("person")[1].childNodes[1].textContent = "Test"
+    // xmlDoc.getElementsByTagName("person")[1].childNodes[1].textContent = "Test"
 
-    const newEle = xmlDoc.createElement("NewQuestion");
+    // const newEle = xmlDoc.createElement("NewQuestion");
 
-    xmlDoc.getElementsByTagName("persons")[0].appendChild(newEle)
+    // xmlDoc.getElementsByTagName("persons")[0].appendChild(newEle)
 
 
 
